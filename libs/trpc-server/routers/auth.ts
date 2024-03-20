3bc6d07ae9c98589e5316f42efbd5f2a3e376220
saranshkhulbe7 <saranshkhulbe7@gmail.com>
2024-03-21 04:17:47 +0530

    prisma removed

--- libs/trpc-server/routers/auth.ts
+++ libs/trpc-server/routers/auth.ts
@@ -1,6 +1,10 @@
 import { publicProcedure, router } from '../trpc';
-import { prisma } from '@upshot/db';
 
 export const authRoutes = router({
-  users: publicProcedure.query(() => prisma.user.findMany()),
+  users: publicProcedure.query(() => [
+    {
+      id: '1',
+      name: 'John Doe',
+    },
+  ]),
 });
